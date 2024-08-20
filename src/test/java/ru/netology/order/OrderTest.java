0fa2ae613406f24f553eebf966f49e4aa37e9a38
Vadim Zhuravlev <vadim.zhuravlev120498@mail.ru>
2024-08-20 12:05:23 +0300

    Update Test

--- src/test/java/ru/netology/order/OrderTest.java
+++ src/test/java/ru/netology/order/OrderTest.java
@@ -3,6 +3,7 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -11,9 +12,10 @@ public class OrderTest {
     @Test
     public void validData(){
         open("http://localhost:9999/");
-        $("[data-test-id=name] input").setValue("Фамилия Имя");
-        $("[data-test-id=phone] input").setValue("+79520000000");
+        $("[data-test-id='name'] input").setValue("Кирсанов Андрей");
+        $("[data-test-id='phone'] input").setValue("+79520000000");
+        $("[data-test-id='agreement']").click();
         $("button.button").click();
-        $("[data-test-id=order-success]").shouldHave(Condition.exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        $("[data-test-id='order-success']").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 }
